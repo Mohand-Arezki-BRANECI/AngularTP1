edb702904a5a00ecdec256a3c717959cf930433e
Mohand Arezki <braniciarezki@gmail.com>
2023-09-14 13:30:05 +0200

    Tp1 Angular, Passing data from Parent to child and vice-versa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ArezkiTP1';
+  public color: string = '';
+  
+  public addItem(newItem: string){
+    this.color = newItem;
+    console.log(this.color);
+    this.changeBgColor(this.color);
+  }
+  private changeBgColor(color : string){
+    document.body.style.backgroundColor = color;
+  }
+  protected colorsArray= ["red","green","blue","yellow"];
 }
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss'],
+
+  
+})
+export class SquareComponent {
+  @Input() bgColor : string = "";
+  
+  @Output() addEventEvent:EventEmitter<string> = new EventEmitter<string>();
+
+  public addEvent(value : string){
+    this.addEventEvent.emit(value);
+  }
+}
